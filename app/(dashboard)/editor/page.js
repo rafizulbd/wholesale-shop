33e9aa8d904087d24218b779ea8d193ad9d038d6
rafizulbd <rafizulbd@gmail.com>
2026-01-17 00:14:47 +0600

    fix: add placeholder to delivery page

---
+++ app/(dashboard)/editor/page.js
@@ -0,0 +1,8 @@
+export default function PlaceholderPage() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-2xl font-bold">এই পেজটির কাজ দ্রুত শুরু হবে</h1>
+      <p className="text-gray-500">বর্তমানে এই সেকশনটি প্রক্রিয়াধীন আছে।</p>
+    </div>
+  );
+}
